770db656f5e3869d46314b292a302eb5ab275823
lucie.bellenger <73110649+Lanuitjedanse@users.noreply.github.com>
2021-06-02 11:38:43 +0200

    fix headline

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <nav className="flex justify-start w-full bg-blue-500 items-center h-full">
       <img className="h-20 ml-5" src="clouds.png" alt="logo" />
-      <h1 className="ml-5 text-white">Cloudify</h1>
+      <h1 className="ml-5 text-white mt-0">Cloudify</h1>
     </nav>
   );
 }
